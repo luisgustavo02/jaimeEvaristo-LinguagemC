c09460cd522030224ef6b49bc31f97d1fb2163c3
Luís Gustavo Martins <54186119+luisgustavo02@users.noreply.github.com>
2022-07-26 16:24:48 -0300

    Create ex1.c

---
+++ capitulo2/exercicios-propostos/ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    
+    float valor = (-(-9) + sqrt((-9) * (-9) - 4*3*6)/(2*3));
+    printf("letra a) %f", valor);
+    
+    int arg = ((pow(3, 2) == 9) && (acos(0) == 0)) || (4 % 8 == 3);
+    printf("letra b) %d", arg); 
+    
+    return 0;
+}
